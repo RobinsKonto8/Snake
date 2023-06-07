f1d68f52bd66940621fc051300f231ba5d1432e9
RobinsKonto8 <130449780+RobinsKonto8@users.noreply.github.com>
2023-06-07 21:08:42 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,3 @@
-// Snake game code
-
 // Initialize canvas
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -11,6 +9,9 @@ let dy = 0;
 let foodX;
 let foodY;
 let score = 0;
+let gameStarted = false;
+let gamePaused = false;
+let gameInterval;
 
 // Generate random food position
 function generateFood() {
@@ -20,11 +21,12 @@ function generateFood() {
 
 // Move the snake
 function moveSnake() {
-  const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
   snake.unshift(head);
   if (snake[0].x === foodX && snake[0].y === foodY) {
     score++;
     generateFood();
+    document.getElementById("score").textContent = "Score: " + score;
   } else {
     snake.pop();
   }
@@ -49,8 +51,8 @@ function checkCollision() {
 }
 
 // Handle keyboard events
-document.addEventListener("keydown", changeDirection);
-function changeDirection(event) {
+document.addEventListener("keydown", handleKeyDown);
+function handleKeyDown(event) {
   if (event.keyCode === 37 && dx !== 10) {
     dx = -10;
     dy = 0;
@@ -63,29 +65,110 @@ function changeDirection(event) {
   } else if (event.keyCode === 40 && dy !== -10) {
     dx = 0;
     dy = 10;
+  } else if (event.keyCode === 80) { // "P" key
+    togglePause();
   }
+}
+
+
+// Show the start screen
+function showStartScreen() {
+  const startScreen = document.getElementById("startScreen");
+  startScreen.classList.add("show");
+  
+  const pauseButton = document.getElementById("pauseButton");
+pauseButton.addEventListener("click", togglePause);
+
+  const startButton = document.getElementById("startButton");
+  startButton.addEventListener("click", startGame);
+}
+
+// Hide the start screen
+function hideStartScreen() {
+  const startScreen = document.getElementById("startScreen");
+  startScreen.classList.remove("show");
+}
+
+// Show the game over screen
+function showGameOverScreen() {
+  const gameOverScreen = document.getElementById("gameOverScreen");
+  gameOverScreen.classList.add("show");
+
+  const scoreText = document.getElementById("scoreText");
+  scoreText.textContent = "Score: " + score;
+
+  const restartButton = document.getElementById("restartButton");
+  restartButton.addEventListener("click", restartGame);
+}
+
+// Hide the game over screen
+function hideGameOverScreen() {
+  const gameOverScreen = document.getElementById("gameOverScreen");
+  gameOverScreen.classList.remove("show");
+}
+
+// Show the pause screen
+function showPauseScreen() {
+  const pauseScreen = document.getElementById("pauseScreen");
+  pauseScreen.classList.add("show");
+
+  const resumeButton = document.getElementById("resumeButton");
+  resumeButton.addEventListener("click", togglePause);
+}
+
+// Hide the pause screen
+function hidePauseScreen() {
+  const pauseScreen = document.getElementById("pauseScreen");
+  pauseScreen.classList.remove("show");
+}
+
+// Restart the game
+function restartGame() {
+  snake = [{x: 200, y: 200}];
+  dx = 10;
+  dy = 0;
+  score = 0;
+  gameStarted = false;
+  gamePaused = false;
+  generateFood();
+  hideGameOverScreen();
+  startGame();
+}
+
+// Start the game
+function startGame() {
+  if (gameStarted) {
+    return;
+  }
+  gameStarted = true;
+  hideStartScreen();
+  generateFood();
+  gameInterval = setInterval(gameLoop, 100);
 }
 
 // Game loop
 function gameLoop() {
+  if (gamePaused) {
+    return;
+  }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   moveSnake();
 
   if (checkCollision()) {
     clearInterval(gameInterval);
-    alert("Game Over!");
+    showGameOverScreen();
     return;
   }
 
   // Draw snake
-  ctx.fillStyle = "#000";
-  snake.forEach((segment) => {
+  ctx.fillStyle = "#00FF00";
+  snake.forEach(segment => {
     ctx.fillRect(segment.x, segment.y, 10, 10);
   });
 
   // Draw food
-  ctx.fillStyle = "#f00";
+  ctx.fillStyle = "#FF0000";
   ctx.fillRect(foodX, foodY, 10, 10);
 
   // Draw score
@@ -94,6 +177,22 @@ function gameLoop() {
   ctx.fillText("Score: " + score, 10, 20);
 }
 
-// Start the game
-generateFood();
-const gameInterval = setInterval(gameLoop, 100);
+// Pause the game
+function togglePause() {
+  if (!gameStarted) {
+    return;
+  }
+
+  if (gamePaused) {
+    hidePauseScreen();
+    gamePaused = false;
+    gameInterval = setInterval(gameLoop, 100);
+  } else {
+    clearInterval(gameInterval);
+    gamePaused = true;
+    showPauseScreen();
+  }
+}
+
+// Start the game when the page loads
+document.addEventListener("DOMContentLoaded", showStartScreen);
